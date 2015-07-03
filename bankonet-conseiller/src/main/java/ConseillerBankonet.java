7f6cbbe47e925518d645758a7a21abbcf6518761
Romain Farau <romain.farau@gmail.com>
2015-07-03 11:11:05 +0200

    Ouverture depot

---
+++ bankonet-conseiller/src/main/java/ConseillerBankonet.java
@@ -0,0 +1,12 @@
+
+public class ConseillerBankonet {
+
+	public void launch(){
+		System.out.println("****** APPLICATION CONSEILLER BANCAIRE ******");
+		
+		
+		
+		System.out.println("Veuillez choisir une action");
+	}
+	
+}
